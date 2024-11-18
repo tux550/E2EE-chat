f1a656b0af862c901b7777a6e91c46ad03710f24
tux550 <rodrigosalad@gmail.com>
2024-11-18 11:59:52 -0500

    tf demo

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,14 @@ provider "aws" {
   region = "us-east-2"
 }
 
+// Sample EC2
+resource "aws_instance" "first_ec2_instance" {
+  ami           = "ami-0942ecd5d85baa812"
+  instance_type = "t2.micro"
+}
+
 // Create documentdb (Free tier)
+/*
 resource "aws_docdb_cluster" "docdb_demo" {
   cluster_identifier = "my-docdb-cluster"
   engine = "docdb"
@@ -31,3 +38,4 @@ resource "aws_docdb_cluster_instance" "docdb_instance" {
   instance_class = "db.t3.medium"
   identifier = aws_docdb_cluster.docdb_demo.id
 }
+*/
